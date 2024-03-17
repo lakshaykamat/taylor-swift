2290537b5447fb1e70e89465e202944b4ab72b97
Lakshay Kamat <lakshaykamat.dev@gmail.com>
2024-03-17 15:48:38 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const { getAlbumByYear, getAlbums } = require("./lib/album");
+const { getSongNames, getRandomSong } = require("./lib/song");
+
+module.exports = {
+  getAlbumByYear,
+  getAlbums,
+  getSongNames,
+  getRandomSong,
+};
