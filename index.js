60bd262eb4944e235ccb326321f1f23027adc216
Lakshay Kamat <lakshaykamat.dev@gmail.com>
2024-03-18 10:01:42 +0530

    Revert "Add Quote"
    This reverts commit a19228bf32db527e0370919ab923638184e2f558.

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const album = require("./lib/album");
 const song = require("./lib/song");
-const quote = require("./lib/quotes");
 
 const taylorAlbums = require("./database/ALBUM_AND_SONGS");
 const taylorSongs = require("./database/SONGS");
@@ -8,7 +7,6 @@ const taylorSongs = require("./database/SONGS");
 const TAYLOR = {
   albums: taylorAlbums,
   songs: taylorSongs,
-  quote,
   album,
   song,
 };
