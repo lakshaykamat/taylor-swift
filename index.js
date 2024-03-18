353645469251a92664b8fbe670afdd3458564c80
Lakshay Kamat <lakshaykamat.dev@gmail.com>
2024-03-18 22:07:22 +0530

    Minor changes

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const album = require("./lib/album");
 const song = require("./lib/song");
+const quote = require("./lib/quotes");
 
 const taylorAlbums = require("./database/ALBUM_AND_SONGS");
-const taylorSongs = require("./database/SONGS");
 
 const TAYLOR = {
   albums: taylorAlbums,
-  songs: taylorSongs,
   album,
+  quote,
   song,
 };
 
 module.exports = TAYLOR;
+module.exports = {
+  song,
+  quote,
+  album
+}
