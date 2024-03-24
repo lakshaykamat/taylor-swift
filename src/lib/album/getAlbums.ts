d09e797f17704af1cba8e6a5ca02ab938db6a2b1
Lakshay Kamat <lakshaykamat.dev@gmail.com>
2024-03-24 19:09:20 +0530

    Minor Changes
    - Code refactor

--- src/lib/album/getAlbums.ts
+++ src/lib/album/getAlbums.ts
@@ -1,13 +1,15 @@
 import ALBUM_AND_SONGS from "../../database/ALBUM_AND_SONGS.json";
 import { Album } from "../../types";
-import { removePropFromAlbum } from "../../utils";
+import { compareReleaseDates, removePropFromAlbum } from "../../utils";
 
 /**
  * Retrieves all albums from the database.
  * @returns {Album[]} An array of albums.
  */
 const getAlbums = (): Album[] => {
-  return ALBUM_AND_SONGS.map(removePropFromAlbum) as Album[];
+  return ALBUM_AND_SONGS.map(removePropFromAlbum).sort(
+    compareReleaseDates
+  ) as Album[];
 };
 
 export default getAlbums;
